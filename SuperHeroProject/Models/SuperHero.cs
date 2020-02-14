19a9a5b3aba70a23bf6bf487afc2d5b5c0804b79
unknown <thetavog@gmail.com>
2020-02-14 14:35:43 -0600

    init

---
+++ SuperHeroProject/Models/SuperHero.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SuperHeroProject.Models
+{
+    public class SuperHero
+    {
+        public int Id { get; set; }
+        public  string Name { get; set; }
+        public string AlterEgo { get; set; }
+        public string PrimaryAbility { get; set; }
+        public string SecondaryHeroProperty { get; set; }
+        public string  CatchPhrase { get; set; }
+
+    }
+}
